1b880bfeebdb42f0715980d39a3bf62726be3eba
Abdul Ahad <ahad83392@gmail.com>
2024-12-12 23:14:46 +0500

    Updated React components and fixed issues

--- src/App.js
+++ src/App.js
@@ -7,18 +7,7 @@ import './Project/Project.css'
 import Portfoilo from './Project/Portfoilo';
 import About from './Project/About';
 import Contact from './Project/Contact';
-// import Navz from './Zameen.com/Navz';
-// import Body from './Zameen.com/Body';
-// import Bottom from './Zameen.com/Bottom';
-// import Calculator from './Zameen.com/Calculator';
-// import './Zameen.com/Zameen.css'
-// import Example from './Zameen.com/Nav';
-// import './Project/Project.css';
-// import Main from './Project/Main';
-// import Portfoilo from './Project/Portfoilo';
-// import About from './Project/About';
-// import Contact from './Project/Contact';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -29,21 +18,6 @@ function App() {
       <About />
       <Contact/>
 
-     
-      {/* <Navz />
-      <Example/>
-      <Body />
-      <Bottom />
-      <Calculator/> */}
-
-      {/* <Main />
-      <Menu />
-      
-      <Portfoilo />
-      
-      <About />
-      
-      <Contact/> */}
 
       
       
--- src/Project/Contact.jsx
+++ src/Project/Contact.jsx
@@ -17,7 +17,17 @@ export default function Contact() {
           </div>
           
         </label>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6844.397207096998!2d71.6640881454102!3d29.382758505467358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2s!4v1729274278185!5m2!1sen!2s" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe 
+  src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6844.397207096998!2d71.6640881454102!3d29.382758505467358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2s!4v1729274278185!5m2!1sen!2s" 
+  width="600" 
+  height="450" 
+  allowfullscreen="" 
+  loading="lazy" 
+  referrerpolicy="no-referrer-when-downgrade" 
+  title="Google Maps Location"
+>
+</iframe>
+
       </div>
       <br />
       <br />
